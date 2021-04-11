62ed52b0803b6ded613522455ad7cca011c0f7d2
MrLeonardDude <leopagnez@gmail.com>
2021-04-11 16:58:07 -0300

    Added a proper reader of name song and band name, but for Azlyrics we still need to use undercase letters

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use reqwest::Error;
 use structopt::StructOpt;
 use std::env;
+use select::document;
+use select::predicate::{*};
 
 #[derive(StructOpt)]
 struct MusidDBCli {
     song_name: String, 
+    band: String,
 
 }
 
@@ -19,16 +22,35 @@ async fn main() -> Result<(), Error> {
         Err(_err) => "".to_string(),
 
     };
+    let song_without_spaces = args.song_name
+        .replace(" ", "")
+        .replace('"', "");
     println!("You chose the song: {}", args.song_name);
     println!("Your choice for db is: {}", cur_db);
 
+    // TODO Minor Case Sensitive
     //  Build Main REqwest at website
-    let res = reqwest::get("http://httpbin.org/get").await?;
+    let res = reqwest::get("https://www.azlyrics.com/lyrics/".to_string()
+        + &*args.band + "/"
+        + &*song_without_spaces + ".html").await?;
     println!("Status: {}", res.status());
     println!("Headers:\n{:#?}", res.headers());
 
+    println!("{}", "https://www.azlyrics.com/lyrics/".to_string()
+    + &*args.band + "/"
+    + &*song_without_spaces + ".html");
+
     // Setting the body to Scrap
     let body = res.text().await?;
-    println!("Body:\n{}", body);
+    // println!("Body:\n{}", body);
+
+    let doc = document::Document::from(&*body); // Great hack for String => &str
+    
+    for node in doc.find(Name("b")) {
+        if args.song_name.eq(&node.text()) { 
+            println!("{}", node.text());
+        }
+    }
+
     Ok(())
 }
